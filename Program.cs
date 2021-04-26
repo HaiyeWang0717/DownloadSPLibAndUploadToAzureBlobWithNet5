51cb2b8370adcffe79019bdf0cf7e220ff7ce104
Haiye Wang <wanghaiye@outlook.com>
2021-04-26 15:28:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace DownloadSPLibAndUploadToAzureBlobWithNet5
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Uri site = new Uri("https://******.sharepoint.com/sites/10000034");
+            string clientId = "";
+            string clientSecret = "";
+
+            // Note: The PnP Sites Core AuthenticationManager class also supports this
+            using (var authenticationManager = new AuthenticationManager())
+            {
+                using (var context = authenticationManager.GetContext(site, clientId, clientSecret))
+                {
+                    context.Load(context.Web, p => p.Title);
+                    await context.ExecuteQueryAsync();
+                    Console.WriteLine($"Title: {context.Web.Title}");
+
+                    var list = context.Web.Lists.GetByTitle("Links");
+                    ListItemCreationInformation itemCreateInfo = new ListItemCreationInformation();
+                    ListItem newItem = list.AddItem(itemCreateInfo);
+                    newItem["Title"] = "My New Item!";
+                    newItem.Update();
+                    await context.ExecuteQueryAsync();
+                    Console.WriteLine(newItem.Id);
+                }
+            }
+
+        }
+    }
+}
